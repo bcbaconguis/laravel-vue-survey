09a147c9b7c690a2c9e183d31f747375d343db53
bcbaconguis <bcbaconguis@gmail.com>
2022-03-25 17:14:45 +0800

    added Question Editor

---
+++ vue/src/components/editor/QuestionEditor.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+
+        <div class="flex items-center justify-between">
+            <h3 class="text-lg font-bold">
+                
+            </h3>
+
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps({
+    question: Object,
+    index: Number,
+
+});
+
+const emit = definteEmits(["change", "addQuestion", "deleteQuestion"]);
+
+const model = ref(JSON.pase(JSON.stringify(props.question)));
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
